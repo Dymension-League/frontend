b0be16dfc55fd5d18ab0c779b6beab98ca1c0998
Nikos Koumbakis <SpiralOutDotEu@gmail.com>
2024-07-18 20:46:43 +0300

    add landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Dymension League</h1>
+      <p>This is the landing page.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
