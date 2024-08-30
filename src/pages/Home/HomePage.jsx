fed44cfa231f308cd987eee3a70a29d00be3728b
a.dacapo21 <aggeloskappos@gmail.com>
2024-08-30 13:20:25 +0200

    FE/Update Landing Page

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import HeaderLeague from "../../components/header/HeaderLeague";
 import NewFooter from "../../components/footer/NewFooter";
 import SliderShips from "../../components/layouts/home-5b/MintShip/SliderShips";
+import SpaceshipAuctions from "../SpaceshipsAuctions";
+import spaceshipsData from "../../assets/space-ships/spaceships";
 
 const HomePage = () => {
     return (
         <div className='home-5'>
             <HeaderLeague />
             <SliderShips />
+            <SpaceshipAuctions data={spaceshipsData} />
             <NewFooter />
         </div>
     );
---
+++ src/pages/SpaceshipsAuctions.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import Countdown from 'react-countdown';
+import CardModal from '../../src/components/layouts/CardModal';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+const SpaceshipAuctions = ({ data }) => {
+    const [modalShow, setModalShow] = React.useState(false);
+
+    const auctionData = data || [];
+
+    return (
+        <section className="tf-section live-auctions">
+            <div className="themesflat-container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="">
+                            <h2 className="tf-title">Spaceships</h2>
+                            <div className="heading-line"></div>
+                        </div>
+                    </div>
+                    <div className="col-md-12">
+                        <Swiper
+                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                            spaceBetween={30}
+                            breakpoints={{
+                                0: {
+                                    slidesPerView: 1,
+                                },
+                                767: {
+                                    slidesPerView: 2,
+                                },
+                                991: {
+                                    slidesPerView: 3,
+                                },
+                                1300: {
+                                    slidesPerView: 4,
+                                },
+                            }}
+                            navigation
+                            pagination={{ clickable: true }}
+                            scrollbar={{ draggable: true }}
+                        >
+                            {auctionData.slice(0, 7).map((item, index) => (
+                                <SwiperSlide key={index}>
+                                    <div className="swiper-container show-shadow carousel auctions">
+                                        <div className="swiper-wrapper">
+                                            <div className="swiper-slide">
+                                                <div className="slider-item">
+                                                    <div className="sc-card-product">
+                                                        <div className="card-media">
+                                                            {/*<Link to="/item-details-01">*/}
+                                                                <video src={item.img} autoPlay loop muted />
+                                                            {/*</Link>*/}
+                                                        </div>
+                                                        <div className="card-title">
+                                                            <h5>{item.name}</h5>
+                                                        </div>
+                                                        <div className="meta-info">
+                                                            <div className="author">
+                                                                <div className="info">
+                                                                    <span>Type</span>
+                                                                    <h6>{item.type}</h6>
+                                                                </div>
+                                                            </div>
+                                                            <div className="author">
+                                                                <div className="info">
+                                                                    <span>Model</span>
+                                                                    <h6>{item.model}</h6>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="card-bottom style-explode">
+                                                            <div className="attribute-item">
+                                                                <span>Color</span>
+                                                                <h6>{item.color}</h6>
+                                                            </div>
+                                                            <div className="attribute-item">
+                                                                <span>Tool</span>
+                                                                <h6>{item.tool}</h6>
+                                                            </div>
+                                                        </div>
+                                                        <div className="card-bottom style-explode">
+                                                            <div className="attribute-item">
+                                                                <span>Capacity</span>
+                                                                <h6>{item.capacity}</h6>
+                                                            </div>
+                                                            <div className="attribute-item">
+                                                                <span>Attack</span>
+                                                                <h6>{item.attack}</h6>
+                                                            </div>
+                                                            <div className="attribute-item">
+                                                                <span>Speed</span>
+                                                                <h6>{item.speed}</h6>
+                                                            </div>
+                                                            <div className="attribute-item">
+                                                                <span>Shield</span>
+                                                                <h6>{item.shield}</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
+            <CardModal show={modalShow} onHide={() => setModalShow(false)} />
+        </section>
+    );
+};
+
+SpaceshipAuctions.propTypes = {
+    data: PropTypes.array.isRequired,
+};
+
+export default SpaceshipAuctions;
